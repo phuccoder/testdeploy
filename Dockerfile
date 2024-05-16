ae1c0e794157ab32cf9869e46cf5c81b6f0569d8
phuccoder <thienphuc8102003@gmail.com>
2024-05-16 13:07:50 +0700

    pp

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM maven:3.8.4-openjdk-11-slim AS build
-COPY . .
-RUN mvn clean package -DskipTests
-
-FROM openjdk:11-jre-slim
-ADD ./demo-0.0.1-SNAPSHOT.jar demo-0.0.1-SNAPSHOT.jar
-ENTRYPOINT ["java","-jar","demo-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/example/diamondstore/DiamondstoreApplication.java
+++ src/main/java/com/example/diamondstore/DiamondstoreApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DiamondstoreApplication {
 
-	public String POST= System.getenv("POST");
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DiamondstoreApplication.class, args);
 	}
